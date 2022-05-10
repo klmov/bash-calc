293d294290277616f8842a4e5831e0b64a1a083c
Kanstantsin Klimov <klimovkostya5@gmail.com>
2022-05-10 15:38:29 +0300

    Update test_calc.py

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -30,7 +30,7 @@ def test_div():
     assert result == 1
 
 def test_ping():
-    hostname = "google.com"
+    hostname = "8.8.8.8"
     response = os.system("ping -c 1 " + hostname)
     # and then check the response...
     if response == 0:
